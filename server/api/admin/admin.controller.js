02f470a557fe915d3398db99a71935a203a2409a
Leigh Hunt <leigh@venari.co.nz>
2015-05-25 22:07:57 +1200

    attempt to restart...

--- server/api/admin/admin.controller.js
+++ server/api/admin/admin.controller.js
@@ -2,6 +2,13 @@
 
 var _ = require('lodash');
 
+var sudo = require('sudo');
+var options = {
+    cachePassword: true,
+    prompt: 'Password, yo? ',
+    spawnOptions: { /* other options for spawn */ }
+};
+
 // Get list of admins
 exports.index = function(req, res) {
   res.json([]);
@@ -10,5 +17,10 @@ exports.index = function(req, res) {
 exports.restart = function(req, res) {
   console.log("RESTART!!!");
   console.log(req.body.action);
+
+	var child = sudo([ 'shutdown', '-r', 'now' ], options);
+	child.stdout.on('data', function (data) {
+	    console.log(data.toString());
+	});
   res.json({restarting:"OK"});
 };
