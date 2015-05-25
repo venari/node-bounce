e6a3309207d4217321827d15d326ab702f66a3fe
Leigh Hunt <leigh@venari.co.nz>
2015-05-25 17:07:57 +1200

    using angular-fullstack:directive

--- client/app/app.js
+++ client/app/app.js
@@ -12,18 +12,4 @@ angular.module('nodeBounceApp', [
       .otherwise('/');
 
     $locationProvider.html5Mode(true);
-  })
-  .directive('ngConfirmClick', [
-        function(){
-            return {
-                link: function (scope, element, attr) {
-                    var msg = attr.ngConfirmClick || "Are you sure?";
-                    var clickAction = attr.confirmedClick;
-                    element.bind('click',function (event) {
-                        if ( window.confirm(msg) ) {
-                            scope.$eval(clickAction)
-                        }
-                    });
-                }
-            };
-    }]);
+  });
---
+++ client/app/ngConfirmClick/ngConfirmClick.directive.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('nodeBounceApp')
+  .directive('ngConfirmClick', function () {
+    return {
+                link: function (scope, element, attr) {
+                    var msg = attr.ngConfirmClick || "Are you sure?";
+                    var clickAction = attr.confirmedClick;
+                    element.bind('click',function (event) {
+                        if ( window.confirm(msg) ) {
+                            scope.$eval(clickAction)
+                        }
+                    });
+                }
+            };
+  });
